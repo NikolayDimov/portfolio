e4bdedea8c728c5eeb995cc64415e8f92baa1e86
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-03-29 14:27:26 +0200

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
 /* Count to a specific number */
 function count(els) {
-	els.each(function () {
-		$(this)
-			.prop('Counter', 0)
-			.animate(
-				{
-					Counter: $(this).text(),
-				},
-				{
-					duration: 3000,
-					easing: 'swing',
-					step: function (now) {
-						$(this).text(Math.ceil(now));
-					},
-				}
-			);
-	});
+    els.each(function () {
+        $(this)
+            .prop("Counter", 0)
+            .animate(
+                {
+                    Counter: $(this).text(),
+                },
+                {
+                    duration: 3000,
+                    easing: "swing",
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    },
+                }
+            );
+    });
 }
 
 /* Count when the counter section is in viewport */
-const counterEls = $('.counter .counter__count');
+const counterEls = $(".counter .counter__count");
 new Waypoint({
-	element: counterEls,
+    element: counterEls,
 
-	handler: function () {
-		count(counterEls);
+    handler: function () {
+        count(counterEls);
 
-		this.destroy();
-	},
+        this.destroy();
+    },
 
-	offset: 'bottom-in-view',
+    offset: "bottom-in-view",
 });
-
-
-
 
 // Sidebar menu with JS
 // function ShowSidebar() {
@@ -61,64 +58,55 @@ new Waypoint({
 // 	}
 // });
 
-
-
-
-
 // Work section - change projects/certificates
 function showSection(sectionId) {
-	const sections = document.querySelectorAll(".work__content");
+    const sections = document.querySelectorAll(".work__content");
 
-	sections.forEach(function (section) {
-		section.classList.remove("visible");
-		section.classList.add("hidden");
-	});
+    sections.forEach(function (section) {
+        section.classList.remove("visible");
+        section.classList.add("hidden");
+    });
 
-	const selectedSection = document.getElementById(sectionId);
-	selectedSection.classList.remove("hidden");
-	selectedSection.classList.add("visible");
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.remove("hidden");
+    selectedSection.classList.add("visible");
 }
 
-
-
 // Send emails
-const contactForm = document.querySelector('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const subject = document.getElementById('subject');
-const message = document.getElementById('message');
-
+const contactForm = document.querySelector("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
 
 function sendEmail() {
-	const bodyMessage = `Name: ${username.value}<br> Email: ${email.value}<br> Message: ${message.value}`;
+    const bodyMessage = `Name: ${username.value}<br> Email: ${email.value}<br> Message: ${message.value}`;
 
-	Email.send({
-		Host: "smtp.elasticemail.com",
-		Username: "",
-		Password: "",
-		To: '',
-		From: "",
-		Subject: subject.value,
-		Body: bodyMessage
-	}).then(
-		message => {
-			if (message == "OK") {
-				Swal.fire({
-					title: "Success!",
-					text: "Message send successfully!",
-					icon: "success"
-				});
-			}
-		}
-	);
-};
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "",
+        Password: "",
+        To: "",
+        From: "",
+        Subject: subject.value,
+        Body: bodyMessage,
+    }).then((message) => {
+        if (message == "OK") {
+            Swal.fire({
+                title: "Success!",
+                text: "Message send successfully!",
+                icon: "success",
+            });
+        }
+    });
+}
 
-contactForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-	sendEmail();
-	username.value = '';
-	email.value = '';
-	subject.value = '';
-	message.value = '';
+    sendEmail();
+    username.value = "";
+    email.value = "";
+    subject.value = "";
+    message.value = "";
 });
