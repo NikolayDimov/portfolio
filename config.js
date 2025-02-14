d72f6d2c2841946f0276bb65ce2a114326b24d9e
Nikolay Dimov <dimovv.nikolay@gmail.com>
2025-02-14 12:03:05 +0200

    fix(contact) email

---
+++ config.js
@@ -0,0 +1,6 @@
+window.env = {
+    APP_EMAIL_USERNAME: "niki9999n@gmail.com",
+    APP_EMAIL_PASSWORD: "6AD2093685EE25D68FD180164E322909289A",
+    APP_EMAIL_TO: "niki9999n@gmail.com",
+    APP_EMAIL_FROM: "niki9999n@gmail.com",
+};
